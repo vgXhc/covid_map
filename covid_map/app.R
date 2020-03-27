56cc3737155b1193bc00169b5dad8bb92f0e103a
vgXhc <kliems@gmail.com>
2020-03-26 22:09:07 -0500

    added "Updated" and credits to map

--- covid_map/app.R
+++ covid_map/app.R
@@ -4,10 +4,14 @@ library(googlesheets4)
 library(tidyverse)
 sheets_deauth()
 df <- read_sheet("https://docs.google.com/spreadsheets/d/1TaghQIUjMTDf1R33BtGtUAwIYjfeoHTUf7wJjMg0O6M/")
+updated <- paste0("Last updated: ",
+                  df$error[1],
+                  " Idea: Heather Pape. Code: Harald Kliems for Madison Bikes. Bike shop list: Wisconsin Bike Fed.")#get time of last update
 
 ui <- fluidPage(
     leafletOutput("mymap"),
     p(),
+    p(updated)
     # actionButton("refresh", "Refresh")
 )
 
@@ -28,4 +32,4 @@ server <- function(input, output, session) {
     })
 }
 
-shinyApp(ui, server)
+shinyApp(ui, server, options = list(height = 1080))
