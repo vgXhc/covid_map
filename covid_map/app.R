be5aced24ea5bb784175f81532877a59caa8d557
vgXhc <kliems@gmail.com>
2020-03-29 12:54:10 -0500

    Small edit to only import needed columns from sheet. May improve performance

--- covid_map/app.R
+++ covid_map/app.R
@@ -5,13 +5,14 @@ library(tidyverse)
 sheets_deauth()
 bfw_link <- a("Wisconsin Bike Fed", href = "http://wisconsinbikefed.org")
 mb_link <- a("Madison Bikes.", href = "https://www.madisonbikes.org")
-df <- read_sheet("https://docs.google.com/spreadsheets/d/1TaghQIUjMTDf1R33BtGtUAwIYjfeoHTUf7wJjMg0O6M/")
+df <- read_sheet("https://docs.google.com/spreadsheets/d/1TaghQIUjMTDf1R33BtGtUAwIYjfeoHTUf7wJjMg0O6M/",
+                 col_types = "c-nncci__cccT_")#loading only relevant variables to improve performance
 updated <- tagList("Last updated: ",
                   df$error[1],
                   " Idea: Heather Pape. Code: Harald Kliems for ", 
                   mb_link, 
                   " Shop list: ",
-                  bfw_link)#get time of last update
+                  bfw_link)#get time of last update and add credit
 
 ui <- fluidPage(
     leafletOutput("mymap"),
